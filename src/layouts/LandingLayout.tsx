ceaef9a887ade141765c173f1b914b09085e6be4
nyun-nye <2001_11_7@naver.com>
2025-10-15 16:14:03 +0900

    refact: 랜딩 레이아웃 header/footer 컴포넌트 사용

--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Button from "@/components/button";
-import mailIcon from "@/assets/ic_mail.svg";
-import xBox from "@/assets/x-box.svg";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function LandingLayout() {
   return (
@@ -11,38 +10,11 @@ export default function LandingLayout() {
         background: "linear-gradient(135deg, #FBEEFF 0.5%, #D1E4FE 100%)",
       }}
     >
-      {/* header 추후에 컴포넌트 분리 예정 */}
-      <header className="flex items-center justify-between px-[80px] py-[16px] bg-opacity-200">
-        <div className="flex items-center gap-[4px]">
-          <img src={xBox} alt="xBox" className="w-[24px] h-[24px]" />
-          <div className="text-h4-primary text-gray-600">DiffLens</div>
-        </div>
-        <div className="flex items-center gap-[16px]">
-          <Button variant="outlined" size="large">
-            로그인
-          </Button>
-          <Button variant="filled" size="large">
-            회원가입
-          </Button>
-        </div>
-      </header>
-
+      <Header isLoggedIn="false" dashboard="false" />
       <main>
         <Outlet />
       </main>
-      {/* footer 추후에 컴포넌트 분리 예정 */}
-      <footer className="flex items-start justify-between px-[80px] py-[40px] bg-primary-100">
-        <div className="flex items-center gap-[4px]">
-          <img src={xBox} alt="xBox" className="w-[24px] h-[24px]" />
-          <div className="text-h4-primary text-gray-600">DiffLens</div>
-        </div>
-        <div className="flex flex-col items-end gap-[8px]">
-          <img src={mailIcon} alt="mail" className="w-[24px] h-[24px]" />
-          <div className="text-body5-secondary text-gray-950">
-            Copyright© 2025. DiffLens. All rights reserved.
-          </div>
-        </div>
-      </footer>
+      <Footer transparency="off" />
     </div>
   );
 }
