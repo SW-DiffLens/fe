214f6ac1aaa99a116ca2d6282cfcf1848025f9b1
nyun-nye <2001_11_7@naver.com>
2025-10-19 20:55:43 +0900

    refact: 랜딩 레이아웃 배경에 원 추가

--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -12,7 +12,15 @@ export default function LandingLayout() {
     >
       <Header isLoggedIn="false" dashboard="false" />
       <main>
-        <Outlet />
+        <div className="w-full h-full z-0 relative">
+          {/* 피그마 시안대로 blur-[197.3px]로 하면 블러 효과가 너무 강해서 blur-3xl로 변경 */}
+          <div className="absolute blur-3xl w-[342px] h-[342px] bg-secondary-100 rounded-full top-[124px] left-[624px]" />
+          <div className="absolute blur-3xl w-[501px] h-[501px] bg-primary-300 rounded-full top-[347px] left-[219px]" />
+          <div className="absolute blur-3xl w-[413px] h-[413px] bg-tertiary-200 rounded-full top-[512px] left-[841px]" />
+        </div>
+        <div className="z-10 relative">
+          <Outlet />
+        </div>
       </main>
       <Footer transparency="off" />
     </div>
