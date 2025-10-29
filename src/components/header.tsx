fe98ede825c799fdc9a8118a1ed97dd999c4f387
nyun-nye <2001_11_7@naver.com>
2025-10-29 22:23:51 +0900

    refact: header 컴포넌트 라이브러리 스타일 변경

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,18 +12,21 @@ import { useState, useRef, useEffect } from "react";
 
 type IsLoggedIn = "true" | "false";
 type Dashboard = "true" | "false";
-type IsPanelLibrary = "true" | "false";
+type IsPanel = "true" | "false";
+type IsLibrary = "true" | "false";
 
 interface HeaderProps {
   isLoggedIn?: IsLoggedIn;
   dashboard?: Dashboard;
-  isPanelLibrary?: IsPanelLibrary;
+  isPanel?: IsPanel;
+  isLibrary?: IsLibrary;
 }
 
 export default function Header({
   isLoggedIn = "false",
   dashboard = "false",
-  isPanelLibrary = "false",
+  isPanel = "false",
+  isLibrary = "false",
 }: HeaderProps) {
   const navigate = useNavigate();
   const [isExportOpen, setIsExportOpen] = useState(false);
@@ -47,7 +50,9 @@ export default function Header({
   }, [isExportOpen]);
   return (
     <div className="flex items-center justify-between px-[80px] py-[16px] bg-opacity-100">
-      <LogoContainer width={128} height={26} />
+      <div onClick={() => navigate("/home")} className="cursor-pointer">
+        <LogoContainer width={128} height={26} />
+      </div>
       {isLoggedIn === "false" && (
         <div className="flex items-center gap-[16px]">
           <Button
@@ -68,59 +73,67 @@ export default function Header({
       )}
       {isLoggedIn === "true" && dashboard === "false" && (
         <div className="flex items-center gap-[16px]">
-          <div className="text-subtitle1 text-primary-700 cursor-pointer">
+          <div
+            className={`text-subtitle1 ${
+              isLibrary === "false" ? "text-primary-700" : "text-gray-700"
+            } cursor-pointer`}
+            onClick={() => navigate("/home")}
+          >
             검색
           </div>
-          <div className="text-subtitle1 text-gray-700 cursor-pointer mr-[8px]">
+          <div
+            className={`text-subtitle1 ${
+              isLibrary === "true" ? "text-primary-700" : "text-gray-700"
+            } cursor-pointer mr-[8px]`}
+            onClick={() => navigate("/home/library")}
+          >
             라이브러리
           </div>
           <MyPageIcon />
         </div>
       )}
-      {isLoggedIn === "true" &&
-        dashboard === "true" &&
-        isPanelLibrary === "false" && (
-          <div className="flex justify-center items-center gap-[16px]">
-            <div className="relative" ref={selectRef}>
-              <Button
-                variant="icon"
-                size="large"
-                onClick={() => setIsExportOpen(!isExportOpen)}
-              >
-                <div>내보내기</div>
-                <ChevronDownIcon color="white" width={16} height={16} />
-              </Button>
-              {isExportOpen && (
-                <ul className="absolute right-0 top-full mt-[12px] inline-block whitespace-nowrap min-w-[180px] bg-white rounded-lg py-[8px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] z-20">
-                  <li
-                    id="mail"
-                    onClick={() => setIsExportOpen(false)}
-                    className="px-[16px] py-[12px] flex items-center gap-[8px] text-body4 text-gray-950 hover:bg-gray-300 cursor-pointer"
-                  >
-                    <MailIcon width={20} height={20} color="#5632A5" />
-                    메일로 전송
-                  </li>
-                  <li
-                    id="csv"
-                    onClick={() => setIsExportOpen(false)}
-                    className="px-[16px] py-[12px] flex items-center gap-[8px] text-body4 text-gray-950 hover:bg-gray-300 cursor-pointer"
-                  >
-                    <FileCsvIcon width={20} height={20} />
-                    .csv로 다운로드
-                  </li>
-                  <li
-                    id="pdf"
-                    onClick={() => setIsExportOpen(false)}
-                    className="px-[16px] py-[12px] flex items-center gap-[8px] text-body4 text-gray-950 hover:bg-gray-300 cursor-pointer"
-                  >
-                    <FilePdfIcon width={20} height={20} />
-                    .pdf로 다운로드
-                  </li>
-                </ul>
-              )}
-            </div>
+      {isLoggedIn === "true" && dashboard === "true" && isPanel === "false" && (
+        <div className="flex justify-center items-center gap-[16px]">
+          <div className="relative" ref={selectRef}>
+            <Button
+              variant="icon"
+              size="large"
+              onClick={() => setIsExportOpen(!isExportOpen)}
+            >
+              <div>내보내기</div>
+              <ChevronDownIcon color="white" width={16} height={16} />
+            </Button>
+            {isExportOpen && (
+              <ul className="absolute right-0 top-full mt-[12px] inline-block whitespace-nowrap min-w-[180px] bg-white rounded-lg py-[8px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] z-20">
+                <li
+                  id="mail"
+                  onClick={() => setIsExportOpen(false)}
+                  className="px-[16px] py-[12px] flex items-center gap-[8px] text-body4 text-gray-950 hover:bg-gray-300 cursor-pointer"
+                >
+                  <MailIcon width={20} height={20} color="#5632A5" />
+                  메일로 전송
+                </li>
+                <li
+                  id="csv"
+                  onClick={() => setIsExportOpen(false)}
+                  className="px-[16px] py-[12px] flex items-center gap-[8px] text-body4 text-gray-950 hover:bg-gray-300 cursor-pointer"
+                >
+                  <FileCsvIcon width={20} height={20} />
+                  .csv로 다운로드
+                </li>
+                <li
+                  id="pdf"
+                  onClick={() => setIsExportOpen(false)}
+                  className="px-[16px] py-[12px] flex items-center gap-[8px] text-body4 text-gray-950 hover:bg-gray-300 cursor-pointer"
+                >
+                  <FilePdfIcon width={20} height={20} />
+                  .pdf로 다운로드
+                </li>
+              </ul>
+            )}
           </div>
-        )}
+        </div>
+      )}
     </div>
   );
 }
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,7 +1,10 @@
 import Header from "@/components/header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function HomeLayout() {
+  const location = useLocation();
+  const isLibrary = location.pathname.includes("/library");
+
   return (
     <div
       className="min-h-screen overflow-hidden"
@@ -10,7 +13,11 @@ export default function HomeLayout() {
       }}
     >
       <main>
-        <Header isLoggedIn="true" dashboard="false" />
+        <Header
+          isLoggedIn="true"
+          dashboard="false"
+          isLibrary={isLibrary ? "true" : "false"}
+        />
         <div className="w-full h-full z-0 relative">
           {/* 피그마 시안대로 blur-[197.3px]로 하면 블러 효과가 너무 강해서 blur-3xl로 변경 */}
           <div className="absolute blur-3xl w-[342px] h-[342px] bg-secondary-100 rounded-full top-[124px] left-[624px]" />
