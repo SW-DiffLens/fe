d2aad729e54581a19d55d232065d0a342bf3d1bd
hyeseong <tell280210@gmail.com>
2025-11-18 02:09:29 +0900

    style: 코드 포맷

--- src/components/dashboard/column-chart.tsx
+++ src/components/dashboard/column-chart.tsx
@@ -1,6 +1,6 @@
 import * as am5 from "@amcharts/amcharts5";
-import * as am5xy from "@amcharts/amcharts5/xy";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import * as am5xy from "@amcharts/amcharts5/xy";
 import { useLayoutEffect, useRef } from "react";
 import { CHART_COLORS } from "@/constants/chart-colors";
 
